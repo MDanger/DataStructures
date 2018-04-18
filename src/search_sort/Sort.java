f8d25f4a834849c298953108abfd904b13355293
MDanger <clearwaters@gmail.com>
2018-04-18 12:41:10 -0700

    added merge sort

--- src/search_sort/Sort.java
+++ src/search_sort/Sort.java
@@ -76,4 +76,52 @@ public class Sort <T extends Comparable<T>> {
         swap(data, min, right);
         return right;
     }
+
+    public static <T extends Comparable<T>> void mergeSort(T[] data){
+        mergeSort(data, 0, data.length - 1);
+    }
+
+    private static <T extends Comparable<T>> void mergeSort(T[] data, int min, int max){
+        if (min < max){
+            int mid = (min + max) / 2;
+            mergeSort(data, min, mid);
+            mergeSort(data, mid+1, max);
+            merge(data, min, mid, max);
+        }
+    }
+
+    private static <T extends Comparable<T>> void merge(T[] data, int first, int mid, int last){
+        T[] temp = (T[]) (new Comparable[data.length]);
+        int first1 = first, last1 = mid;
+        int first2 = mid+1, last2 = last;
+        int index = first1;
+
+        while (first1 <= last1 && first2 <= last2){
+            if (data[first1].compareTo(data[first2]) <0){
+                temp[index] = data[first1];
+                first1++;
+            } else {
+                temp[index] = data[first2];
+                first2++;
+            }
+            index++;
+        }
+        while(first1 <= last1){
+            temp[index] = data[first1];
+            first1++;
+            index++;
+        }
+
+        while (first2 <= last2){
+            temp[index] = data[first2];
+            first2++;
+            index++;
+        }
+
+        for (index = first; index <= last; index++){
+            data[index] = temp[index];
+        }
+    }
+
+    
 }
